5a10e704e4dadaf547b78631f734ccbe1fd62657
Nguyễn Thanh Chính <73381800+Citnut@users.noreply.github.com>
2022-01-23 18:25:47 -0800

    update 24/01/2022

--- bot/tools.js
+++ bot/tools.js
@@ -1,25 +1,34 @@
-const fs = require('fs');
+const fs = require('fs')
 
 const getParam = text => {
 	return text
 		.split(' ')
 		.slice(1)
-		.join(' ');
-};
+		.join(' ')
+}
 
 const getKeyword = text => {
 	return text
 		.split(' ')
 		.slice(0, 1)[0]
-		.slice(1);
-};
+		.slice(1)
+}
 
 const getFile = filePath => {
 	return fs.createReadStream(filePath)
-};
+}
+
+const execShellCommand = (cmd) => {
+    return new Promise((resolve) => {
+        childProcess.exec(cmd, (error, stdout, stderr) =>
+            resolve(stdout ? stdout : stderr)
+        )
+    })
+}
 
 module.exports = {
 	getParam,
 	getKeyword,
-	getFile
+	getFile,
+	execShellCommand
 }
