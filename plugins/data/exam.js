b53dc6922d715cdc8d01fc24f054424ae6ca2f9c
Nguyễn Thanh Chính <73381800+Citnut@users.noreply.github.com>
2022-07-28 07:34:03 +0000

    Update exam.js

--- plugins/data/exam.js
+++ plugins/data/exam.js
@@ -8,7 +8,7 @@ module.exports = {
     permission: false,// admin | adminsv | false
     allowInteraction: false,//tương tăc khác, vd: các nút ấn
 	allowListening: false,//luôn lắng nghe (noprefix)
-    async interaction (data) {
+    async interaction (data, db) {
         let {customId} = data
         if (!data.isButton()) return
 		//code
@@ -17,17 +17,7 @@ module.exports = {
         //code (noprefix)
 	},
 	async call (data,db) {
-		/*
-		db là database của bot bao gồm get và write
-		get db: db.get //>Object
-		write db: db.write(data) //>data: Object
-		thay đổi dữ liệu trong db:
-		<code>
-			let {get,write} = db
-			get.abc = abc
-			write(get)
-		<code>
-		*/
+		
         //code khi được gọi đến
 	}
 }
