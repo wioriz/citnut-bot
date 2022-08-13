9d45f0d64ad4a63f71f9fc2625eed45a7bae7fbd
Nguyễn Thanh Chính <73381800+Citnut@users.noreply.github.com>
2022-08-13 19:11:56 +0700

    uptime

--- plugins/uptime.js
+++ plugins/uptime.js
@@ -14,44 +14,49 @@ module.exports = {
 		.setName(command[0])
 		.setDescription(description)
 	,
-	async slashHandle (data, db) {return await this.call(data,db,data.user.id)},
+	async slashHandle (data, db) {
+		let res = await citnut.tools.getapi("girl",data,false)
+		return data.reply(await this.upt(db,data.user.id,res,data.createdTimestamp))
+	},
 	async listen (data,db) {
 	},
-	async call (data,db,id) {
-		let price = citnut.config.price.uptime
+	async call (data,db) {			
+		let res = await citnut.tools.getapi("girl",data,false)
+		return data.reply(await this.upt(db,data.author.id,res,data.createdTimestamp))
+	},
 
-		if(db.user[id?id:data.author.id].money<price) {
-			let thieutien = "bạn còn thiếu "+(price-db.user[id?id:data.author.id].money)+" 💵 để sử dụng lệnh này"
-			return data.reply({embeds:[citnut.defaultemb(thieutien)],allowedMentions:citnut.allowedMentions})
-		}else {
-			db.user[id?id:data.author.id].money-=price
-		}
-
-		let prefix = citnut.config.prefix,
-			time = process.uptime(),
-			day = Math.floor(time/(60*60*24)),
-			hours = Math.floor((time / (60 * 60)) - (day*24)),
-			minutes = Math.floor((time % (60 * 60)) / 60),
-			seconds = Math.floor(time % 60),
-			timeStart = Date.now(),
-			ram = (totalmem-freemem)/1024/1024,
-			total = db.total
+	async upt (db,id,res,ping) {
 		try {
-			let res = await citnut.tools.getapi("girl",data,false)
+			let price = citnut.config.price.uptime
 
-			let hoatdong = ""
+			if(db.user[id].money<price) {
+				let thieutien = "bạn còn thiếu "+(price-db.user[id].money)+" 💵 để sử dụng lệnh này"
+				return {embeds:[citnut.defaultemb(thieutien)],allowedMentions:citnut.allowedMentions}
+			}else {
+				db.user[id].money-=price
+			}
+
+			let prefix = citnut.config.prefix,
+				time = process.uptime(),
+				day = Math.floor(time/(60*60*24)),
+				hours = Math.floor((time / (60 * 60)) - (day*24)),
+				minutes = Math.floor((time % (60 * 60)) / 60),
+				seconds = Math.floor(time % 60),
+				ram = (totalmem-freemem)/1024/1024,
+				total = db.total,
+				hoatdong = "";
+
 			hoatdong+=(day>0)?`${day} ngày\n`:""
 			hoatdong+=(hours>0)?`${hours} giờ\n`:""
 			hoatdong+=(minutes>0)?`${minutes} phút\n`:""
 			hoatdong+=seconds+" giây"
-			db.total.user = Object.keys(db.user).length + 1
+			db.total.user = Object.keys(db.user).length
 			 
-			const emb = citnut.defaultemb(`bot đã hoạt động được:\n${hoatdong}\n> Prefix: ${prefix}\n> Tổng số tin nhắn: ${total.msg}\n> Tổng số người dùng: ${total.user}\n> Ram đang sử dụng: ${ram.toFixed(1)}MB\n> Ping: ${Date.now() - timeStart}ms`)
+			const emb = citnut.defaultemb(`bot đã hoạt động được:\n${hoatdong}\n> Prefix: ${prefix}\n> Tổng số tin nhắn: ${total.msg}\n> Tổng số người dùng: ${total.user}\n> Ram đang sử dụng: ${ram.toFixed(1)}MB\n> Ping: ${Date.now() - ping}ms`)
 	
-			return data.reply({embeds:[!res?emb:emb.setThumbnail(res)],allowedMentions:citnut.allowedMentions})		
+			return {embeds:[!res?emb:emb.setThumbnail(res)],allowedMentions:citnut.allowedMentions}	
 		}catch (e) {
-			send(`đã xảy ra lỗi`, data)
-			console.error(e)
+			return `đã xảy ra lỗi`
 		}
 	}
 }
